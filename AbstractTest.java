893af10f6cc2527f43b26777bdb26405b7679037
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-01-04 09:21:22 +0530

    OUT DOOR GAME

--- AbstractTest.java
+++ AbstractTest.java
@@ -1,5 +1,16 @@
 package com.lti;
 
+
+/*
+ * 
+ * 				InDoorGame				OutDoorGame
+ * 					|						|
+ * 			-----------------		--------------------
+ * 			|		|		|		|		 |		 |
+ * 		TableTennis Chess Carrom	FootBall Cricket Hockey
+ * 
+ * 
+ */
 public class AbstractTest {
 	public static void main(String[] args) {
 		//InDoorGame idg = new InDoorGame();
@@ -26,6 +37,32 @@ public class AbstractTest {
 		tt.findSunRoofing();
 		tt.getSomeSnacks();
 		tt.calculateScore();
+		
+		System.out.println("-------");
+		
+		Cricket crik = new Cricket();
+		crik.hitSixer();
+		crik.findSunLight();
+		crik.getSomeColdDrinks();
+		crik.calculateScore();
+		
+		System.out.println("-------");
+		
+		FootBall fb = new FootBall();
+		fb.hitGoal();
+		fb.findSunLight();
+		fb.getSomeColdDrinks();
+		fb.calculateScore();
+		
+		System.out.println("-------");
+		
+		Hockey hk = new Hockey();
+		hk.hitHockeyStick();
+		hk.findSunLight();
+		hk.getSomeColdDrinks();
+		hk.calculateScore();
+		
+				
 		
 	}
 }
@@ -80,6 +117,44 @@ abstract class InDoorGame // cannot be instantiated
 	}
 	
 }
+
+abstract class OutDoorGame // cannot be instantiated  
+{
+	int score;
+	int numberOfPlayers;
+	
+	//it is a mandate on the immediate non-abstract child to implement it
+	
+	//below is just a declaration
+	abstract void calculateScore(); //code body is undefined
+		// but now it "has to be" / "bound to be" defined by the immediate 
+		//non-abstract child
+//The abstract method calculateScore in type InDoorGame can only be 
+	//defined by an abstract class
+	
+	OutDoorGame() {
+		System.out.println("OutDoorGame() .....");
+	}
+	public OutDoorGame(int score, int numberOfPlayers) {
+		super();
+		this.score = score;
+		this.numberOfPlayers = numberOfPlayers;
+	}
+	void findSunLight() {
+		System.out.println("ample sunlight..to play outdoor game....");
+	}
+	
+	
+	void getSomeColdDrinks() {
+		System.out.println("Get some cold drinks / juices...");
+	}
+	
+	@Override
+	public String toString() {
+		return "InDoorGame [score=" + score + ", numberOfPlayers=" + numberOfPlayers + "]";
+	}
+	
+}
 //The type TableTennis must implement the inherited abstract method
 //InDoorGame.calculateScore()
 class TableTennis extends InDoorGame
@@ -116,3 +191,38 @@ class Carrom extends InDoorGame
 		System.out.println("Calculating who is the carrom winner....");
 	}
 }
+
+
+class FootBall extends OutDoorGame
+{
+	void hitGoal() { 
+		System.out.println("FootBall : hitting..goal.");
+	}
+	
+	void calculateScore() {
+		System.out.println("Calculating FootBall score...");
+	}
+}
+
+class Cricket extends OutDoorGame
+{
+	void hitSixer() { 
+		System.out.println("Cricket : hitting sixer...");
+	}
+	
+	void calculateScore() {
+		System.out.println("Calculating cricket score..");
+	}
+}
+
+class Hockey extends OutDoorGame
+{
+	void hitHockeyStick() { 
+		System.out.println("Hockey : hitting the hockey stick..");
+	}
+	
+	void calculateScore() {
+		System.out.println("Calculating who is the Hockey winner....");
+	}
+}
+
