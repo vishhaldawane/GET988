9959ca6712617a69bbaecdc5fb873f14c29c0dbf
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-01-04 15:27:37 +0530

    RED SIGNAL DISHONOURED EXCEPTION

--- CustomeExceptionTest.java
+++ CustomeExceptionTest.java
@@ -6,7 +6,16 @@ public class CustomeExceptionTest {
 		myCar.startTheCar();
 	}
 }
-
+class RedSignalDishnouredException extends RuntimeException{ //user defined exception
+	RedSignalDishnouredException(String msg) {
+		super(msg);
+	}
+}
+class TyrePuncturedException extends RuntimeException{
+	TyrePuncturedException(String msg) {
+		super(msg);
+	}
+}
 class Car
 {
 	void startTheCar() {
@@ -16,30 +25,35 @@ class Car
 		{
 			longDrive(); //Unhandled exception type Exception
 		}
-		catch(Exception e) {
-			System.out.println(e.getMessage());
+		catch(TyrePuncturedException e) {
+			System.out.println("Problem1 : "+e.getMessage());
+		}
+		catch(RedSignalDishnouredException e) {
+			System.out.println("Problem2 : "+e.getMessage());
 		}
 	}
 	
 	// checked exceptions must be declared in the THROWS clause of the method
 	
-	void longDrive()  throws Exception //
+	void longDrive() 
 	{
 		for(int i=1;i<=25;i++) {
 			System.out.println(i+" Km completed");
 			double value = Math.random()%10;
 			//System.out.println("value "+value);
 			if(value > 0.75 && value < 0.80 ) {
-				Exception rte = new Exception("Oh No!!! Red Signal is dishonoured...");
+			//	Exception rte = new Exception("Oh No!!! Red Signal is dishonoured...");
 			//	RuntimeException rte = new RuntimeException("Oh No!!! Red Signal is dishonoured...");
-				throw rte;
+				RedSignalDishnouredException rsdhEx = new RedSignalDishnouredException("Oh No!!! Red SIGNAL is dishonoured...");
+				
+				throw rsdhEx;
 			}
-			
-			
+	
 			if(value > 0.10 && value < 0.15) {
-				Exception rte = new Exception("Oh No!!! One of the tyre is punctured ..");
+			//	Exception rte = new Exception("Oh No!!! One of the tyre is punctured ..");
 			//	RuntimeException rte = new RuntimeException("Oh No!!! One of the tyre is punctured ..");
-				throw rte;
+				TyrePuncturedException tpEx = new TyrePuncturedException("Oh No!!! One of the tyre is punctured ..");
+				throw tpEx;
 			}
 			
 		}
