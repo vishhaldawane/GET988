40d31be0597fa8e8e13910806887afde440ff6e3
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-01-11 17:12:10 +0530

    IMAGES AND ADDRESS

--- BankAccount.ts
+++ BankAccount.ts
@@ -2,9 +2,24 @@ export class BankAccount {
     accountNumber: number;
     accountHolder: string;
     accountBalance: number;
+    isActive: boolean;
+    image: string;
+    address : Address = new Address();
+
     constructor() {
         this.accountNumber=10;
         this.accountHolder="Jack";
         this.accountBalance=5000;
+        this.isActive=true;
+        this.image = '';
     }
+}
+
+class Address {
+    area: string='';
+    street: string='';
+    city: string='';
+    state: string='';
+    country: string='';
+    pin : string=''; 
 }
--- bank-account.component.ts
+++ bank-account.component.ts
@@ -10,9 +10,55 @@ export class BankAccountComponent implements OnInit {
   baObj: BankAccount = new BankAccount();
 
   baObjArray: BankAccount[] = [
-    {accountNumber:101,accountHolder:"Julie",accountBalance:5000, isActive:true},
-    {accountNumber:102,accountHolder:"Janet",accountBalance:6000, isActive:true},
-    {accountNumber:103,accountHolder:"Jennie",accountBalance:7000, isActive:true}
+      {
+        accountNumber:101,
+        accountHolder:"Bruce",
+        accountBalance:5000, 
+        isActive:true,
+        image:'assets/bruce.jpeg',
+        address:{
+          area:"Ram Mahal",
+          street:"West Avenue",
+          city:"Mumbai",
+          state:"Maharashtra",
+          country:"India",
+          pin:"401501"
+        }
+
+      },
+
+      {
+        accountNumber:102,
+        accountHolder:"James",
+        accountBalance:6000, 
+        isActive:true,
+        image:'assets/james.jpeg',
+        address:{
+          area:"Raheem Mnzil",
+          street:"East Avenue",
+          city:"Pune",
+          state:"Maharashtra",
+          country:"India",
+          pin:"501601",
+        },
+
+      },
+
+      {
+        accountNumber:103,
+        accountHolder:"Steve",
+        accountBalance:7000, 
+        isActive:true,
+        image:'assets/steve.jpeg',
+        address:{
+          area:"Happy Home",
+          street:"North Avenue",
+          city:"Chennai",
+          state:"Tamilnadu",
+          country:"India",
+          pin:"601701"
+        },
+      }
   ];
 
   constructor() { }
