06f82b5b8d676da0be4c0b569ec35331de5f5a64
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-01-11 15:57:04 +0530

    ARRAY

--- bank-account.component.ts
+++ bank-account.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { BankAccount } from 
-'./BankAccount';
+import { BankAccount } from './BankAccount';
+
 @Component({
   selector: 'app-bank-account',
   templateUrl: './bank-account.component.html',
@@ -9,9 +9,18 @@ import { BankAccount } from
 export class BankAccountComponent implements OnInit {
   baObj: BankAccount = new BankAccount();
 
+  baObjArray: BankAccount[] = [
+    {accountNumber:101,accountHolder:"Julie",accountBalance:5000, isActive:true},
+    {accountNumber:102,accountHolder:"Janet",accountBalance:6000, isActive:true},
+    {accountNumber:103,accountHolder:"Jennie",accountBalance:7000, isActive:true}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+      this.baObj.accountNumber=100;
+      this.baObj.accountHolder="Smith";
+      this.baObj.accountBalance=5555;
   }
 
 }
